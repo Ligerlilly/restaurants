5568dca9e388226c38a9604bce78626bfb81b5df
Jason <quizathaderat@yahoo.com>
2015-08-24 16:56:06 -0700

    Initial commit

---
+++ js/RestaurantsController.js
@@ -0,0 +1,5 @@
+restaurantList.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
+  $scope.restaurants = [
+   { name: 'Andina', type: 'Peruvian', location: 'Pearl, price_range: '$$$' }
+ ];
+});
