9a9e13ff2f671b22eb183afc690da1fc21caaede
Jason <quizathaderat@yahoo.com>
2015-08-24 19:26:06 -0700

    add new restaurant form

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,5 +1,9 @@
 restaurantList.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
   $scope.restaurants = [
-   { name: 'Andina', type: 'Peruvian', location: 'Pearl, price_range: '$$$' }
+   { name: 'Andina', type: 'Peruvian', location: 'Pearl', price: '$$$' }
  ];
+ $scope.addRestaurant = function() {
+   $scope.restaurants.push({name: $scope.restaurantName, type: $scope.restaurantType, location: $scope.restaurantLocation, price: $scope.restaurantPrice});
+   $scope.restauarantName = null;
+ };
 });
